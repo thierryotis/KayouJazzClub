1c960f5176c37ce23fb3b825f0f3df1ecdc2ecc5
thierryotis <thierryotis@yahoo.fr>
2025-01-15 11:31:59 -0500

    fin des modifications

--- src/Components/programs/Programs.jsx
+++ src/Components/programs/Programs.jsx
@@ -2,44 +2,42 @@ import React from "react";
 import "./Programs.css";
 import Program_1 from "../../assets/kayou.jpg";
 import Program_2 from "../../assets/nkoumouyannick.jpg";
-import white_arrow from '../../assets/white-arrow.png'
-
+import white_arrow from "../../assets/white-arrow.png";
 
 const Programs = () => {
   return (
     <>
-    <div className="programs team">
-      <div className="program">
-        <img src={Program_1} alt="" />
-        <div className="caption">
-          <h2>Kayou</h2>
-          <p>
-            Kayou, a renowned Cameroonian saxophonist, founded Kayou Jazz Club
-            with a mission: to use music as a tool for social change. A graduate
-            of the Conservatoire of Cergy-Pontoise in France, Kayou has
-            performed with legends like Manu Dibango and Youssou N'Dour. In
-            2016, he launched The Orchestra at School, bringing music education
-            to underprivileged children in Cameroon, fostering life skills like
-            confidence and teamwork.
-          </p>
+      <div className="programs team">
+        <div className="program">
+          <img src={Program_1} alt="" />
+          <div className="caption">
+            <h2>Kayou</h2>
+            <p>
+              A celebrated Cameroonian saxophonist and graduate of the
+              Conservatoire of Cergy-Pontoise in France, founded Kayou Jazz Club
+              to harness music for social change. Having performed with icons
+              like Manu Dibango and Youssou N'Dour, he launched The Orchestra at
+              School in 2016, providing music education to underprivileged
+              children in Cameroon and nurturing skills like confidence and
+              teamwork.
+            </p>
+          </div>
+        </div>
+        <div className="program">
+          <img src={Program_2} alt="" />
+          <div className="caption">
+            <h2>Yannick Noumou</h2>
+            <p>
+              Joining him is Yannick Nkoumou, a Senior Consultant in social and
+              behavior change, who co-founded the club, convinced of music’s
+              power to drive Africa’s development. With expertise in strategic
+              management, Yannick ensures the smooth running of all projects,
+              blending his passion for art with social impact.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="program">
-        <img src={Program_2} alt="" />
-        <div className="caption">
-          <h2>Yannick Noumou</h2>
-          <p>
-            Joining him is Yannick Nkoumou, a Senior Consultant in social and
-            behavior change, who co-founded the club, convinced of music’s power
-            to drive Africa’s development. With expertise in strategic
-            management, Yannick ensures the smooth running of all projects,
-            blending his passion for art with social impact.
-          </p>
-        </div>
-      </div>
-      
-    </div>
-    {/*<button className='btn dark-btn pg'>Join Us <img src={white_arrow} alt=""/></button>*/}
+      {/*<button className='btn dark-btn pg'>Join Us <img src={white_arrow} alt=""/></button>*/}
     </>
   );
 };
